9c65fd4d70ca94cf574ce5255f73a585f6220cda
Arthur Auersvald <117219036+ArthurAuersvald@users.noreply.github.com>
2025-09-17 23:34:36 +0000

    Criação de entidade Usuario

---
+++ projeto-integrador-easy/src/main/java/entidades/Usuario.java
@@ -0,0 +1,16 @@
+package entidades;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
