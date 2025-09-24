4ac61f13d4cf8c180525119a8c10f9bd5130a83e
Emanuel Luiz <106766799+emanuelluiz01@users.noreply.github.com>
2025-09-24 13:59:07 +0000

    feat: create Categoria]Test

---
+++ projeto-integrador-easy/src/main/java/entidades/test/CategoriaTest.java
@@ -0,0 +1,24 @@
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+
+@QuarkusTest
+public class CategoriaTest {
+
+    @Test
+    @Transactional
+    public void testSalvaUmaCategoria() {
+        Categoria categoria = new Categoria();
+        categoria.nome = "Alimentacao";
+        categoria.tipo = "Casa";
+
+        categoria.persist();
+        Assertions.assertNotNull(categoria.id);
+        
+        Optional<Categoria> categoriaSalva = Categoria.findByIdOptional(categoria.id);
+        Assertions.assertTrue(categoriaSalva.isPresent());
+        Assertions.assertEquals("Alimentacao", categoriaSalva.get().nome);
+    }
+}
